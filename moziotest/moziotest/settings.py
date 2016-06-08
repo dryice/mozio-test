b7de7da0021601bb8ba1dcaee605c0698de74ab7
Dryice Liu <dryiceliu@gmail.com>
2016-06-08 14:08:45 +0800

    the user login/logout/registration etc via django-rest-auth, django-allauth, and django-rest-framework

--- moziotest/moziotest/settings.py
+++ moziotest/moziotest/settings.py
@@ -37,6 +37,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
+
+    'rest_framework',
+    'rest_framework.authtoken',
+    'allauth',
+    'allauth.account',
+    'rest_auth',
+    'rest_auth.registration',
 ]
 
 INSTALLED_APPS += local_settings.LOCAL_INSTALLED_APPS
@@ -123,3 +131,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+SITE_ID = 1
