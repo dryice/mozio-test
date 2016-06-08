26b8ac9b378c7ec925d955740c938e4fc22019c2
Dryice Liu <dryiceliu@gmail.com>
2016-06-09 00:00:36 +0800

    permissions:
    - one can only modify/delete his/her own service area
    - anonymous user view only (no post to create new service area)

--- moziotest/userprofile/views.py
+++ moziotest/userprofile/views.py
@@ -1,7 +1,23 @@
 from rest_framework import generics, viewsets
+from rest_framework import permissions
 
 from .models import ServiceArea
 from .serializers import ServiceAreaSerializer
+
+
+class IsOwnerOrReadOnly(permissions.BasePermission):
+    """
+    Custom permission to only allow owners of an object to edit it.
+    """
+
+    def has_object_permission(self, request, view, obj):
+        # Read permissions are allowed to any request,
+        # so we'll always allow GET, HEAD or OPTIONS requests.
+        if request.method in permissions.SAFE_METHODS:
+            return True
+
+        # Write permissions are only allowed to the owner of the snippet.
+        return obj.provider == request.user
 
 # class ServiceAreaList(generics.ListCreateAPIView):
 #     queryset = ServiceArea.objects.all()
@@ -14,6 +30,8 @@ from .serializers import ServiceAreaSerializer
 class ServiceAreaViewSet(viewsets.ModelViewSet):
     queryset = ServiceArea.objects.all()
     serializer_class = ServiceAreaSerializer
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,
+                          IsOwnerOrReadOnly,)
     
     def perform_create(self, serializer):
         serializer.save(provider=self.request.user)
